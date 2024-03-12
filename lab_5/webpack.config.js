2dfa2bdcd1c082947ab9dd7bc50725dc56aea93c
NotAllRight <maks.kvitko.333@gmail.com>
2024-03-12 21:49:18 +0300

    update

--- lab_5/webpack.config.js
+++ lab_5/webpack.config.js
@@ -1,52 +1,62 @@
 // webpack.config.js
-const path = require("path")
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development', // устанавливаем режим development
-    entry: {
-        main: path.resolve(__dirname, "./src/index.js"),
+  mode: "development", // устанавливаем режим development
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"), // шлях до папки зі збіркою
     },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, "./dist"), // шлях до папки зі збіркою
-        },
-        compress: true, // включає стиснення
-        port: 9000, // порт для dev server
-        open: true, // відкривати браузер автоматично
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: path.resolve(__dirname, './src/pages/index.html'), // шаблон
-            filename: 'index.html', // ім'я вихідного файлу
-        }),
-        new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: path.resolve(__dirname, './src/pages/rozklad.html'), // шаблон
-            filename: 'rozklad.html', // ім'я вихідного файлу
-        }),
-        new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: path.resolve(__dirname, './src/pages/photo.html'), // шаблон
-            filename: 'photo.html', // ім'я вихідного файлу
-        }),
-        new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: path.resolve(__dirname, './src/pages/news.html'), // шаблон
-            filename: 'news.html', // ім'я вихідного файлу
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-              { from: 'src/assets/images', to: 'assets/images' }
-            ],
-        }),
+    compress: true, // включає стиснення
+    port: 9000, // порт для dev server
+    open: true, // відкривати браузер автоматично
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/pages/index.html"), // шаблон
+      filename: "index.html", // ім'я вихідного файлу
+    }),
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/pages/rozklad.html"), // шаблон
+      filename: "rozklad.html", // ім'я вихідного файлу
+    }),
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/pages/photo.html"), // шаблон
+      filename: "photo.html", // ім'я вихідного файлу
+    }),
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/pages/news.html"), // шаблон
+      filename: "news.html", // ім'я вихідного файлу
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/assets/images", to: "assets/images" }],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
+  },
 };
